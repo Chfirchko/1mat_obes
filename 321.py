63d34b2f931978816fb3c6ffe424a271dfd1a7fc
Chfirchko <anatol30@list.ru>
2023-02-16 01:01:15 +0300

    v. 0.1 допиливать и допиливать

---
+++ 321.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class CoordRow:
+    def __init__(self, vector, funcValue):
+        self.vector = np.array
+        self.funcValue = funcValue
+
+    def getVector(self):
+        return self.vector
+
+    def getFuncValue(self):
+        return self.funcValue
